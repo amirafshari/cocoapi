762fd5539a5229470223ebae48b197fffe15df51
amirafshari <4mir4fshari@gmail.cim>
2023-01-13 17:53:23 +0330

    bugs fixed

--- filter.py
+++ filter.py
@@ -32,9 +32,11 @@ def catFinder(cat_ids, coco_json_path, new_ann_path):
                 new_annotations[key][0]['height'] = img['height']
                 new_annotations[key][0]['width'] = img['width']
     
-    json = json.dumps(new_annotations)
+    json_file = json.dumps(new_annotations)
     f = open(new_ann_path,"w")
-    f.write(json)
+    f.write(json_file)
+    
+    print('Found all images with your categories :)')
         
     return new_annotations
 
@@ -48,9 +50,10 @@ def copyImgs(new_ann_path, new_img_path):
     f = open(new_ann_path)
     data = json.load(f)
     for e in data.values():
-        print(e['file_name'])
-        file_name = e['file_name']
+        file_name = e[0]['file_name']
         shutil.copy(os.path.join('train2017', file_name), os.path.join(new_img_path, file_name))
+    
+    print('Copied all images with those categories to the new directory')
 
 
 def toYOLO(data, yolo_ann_path):
@@ -71,18 +74,20 @@ def toYOLO(data, yolo_ann_path):
             
         np.savetxt(os.path.join(yolo_ann_path,e['file_name'][:-4]+'.txt'), e['bbox'], fmt='%.10g')
 
-
+    
+    print('All annotations converted to YOLO format')
+    print('Done!')
 
 
 # [bird,cat,dog, backpack,umbrella,handbag,suitcase, chair,couch,dining table, ,laptop,cellphone,book]
 cat_ids = [2,3,4,6,8, 16,17,18, 27,28,31,33, 62,63,69, 73,77, 84] 
-new_ann_path = 'new_instances_train2017.json'
-coco_json_path = 'instances_train2017.json'
+new_ann_path = 'annotations/new_instances_train2017.json'
+coco_json_path = 'annotations/instances_train2017.json'
 
 data = catFinder(cat_ids, coco_json_path,  new_ann_path)
         
-new_img_path = 'new_train'
+new_img_path = 'new_train2017'
 copyImgs(new_ann_path, new_img_path)    
 
 yolo_ann_path = 'yolo'
-toYOLO(data)
+toYOLO(data, yolo_ann_path)
